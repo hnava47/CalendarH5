014a3b21bea62e59394a462cb433fe638e775764
Hector Nava <hector.r.nava44@gmail.com>
2021-11-17 15:25:28 -0700

    Added document.ready function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,62 +1,73 @@
-const $currDayEl = $('#currentDay');
-const $timeBlockEl = $('#time-block');
-const currMoment = moment().hour();
-// let meetings = JSON.parse(localStorage.getItem('meetings')) || [];
+$(document).ready(function() {
+    const $currDayEl = $('#currentDay');
+    const $timeBlockEl = $('#time-block');
+    const currMoment = moment().hour();
+    let meetings = JSON.parse(localStorage.getItem('meetings')) || [];
 
-// inputs current date based on locale
-$currDayEl.text(moment().format('dddd, MMMM Do'));
+    // inputs current date based on locale
+    $currDayEl.text(moment().format('dddd, MMMM Do'));
 
-// let currentMeet = {
-//     time: '9am',
-//     details: 'test'
-// };
+    // let currentMeet = {
+    //     time: '9am',
+    //     details: 'test'
+    // };
 
-// meetings.push(currentMeet);
-// meetings.sort((a, b) => b.time - a.time);
+    // meetings.push(currentMeet);
+    // meetings.sort((a, b) => b.time - a.time);
 
-// localStorage.setItem('meetings', JSON.stringify(meetings));
+    // localStorage.setItem('meetings', JSON.stringify(meetings));
 
-homePage();
+    homePage();
 
-function homePage() {
-    for (let i = 9; i < 18; i++) {
-        let currHour = 0;
-        if (i > 12) {
-            currHour += i - 12;
-            currHour = currHour + 'pm';
-        } else {
-            currHour += i;
-            currHour = currHour + 'am';
-        }
+    function updateStorage(event) {
+        event.preventDefault();
 
-        const $rowEl = $('<div>');
-        const $calTimeEl = $('<div>');
-        const $calTextEl = $('<textarea>');
-        const $calSubmitEl = $('<div>');
-        const $submitIconEl = $('<i>');
 
-        $calTimeEl.addClass('col-1 hour')
-            .text(currHour);
+    }
 
-        $calTextEl.addClass('col-10 description');
+    function homePage() {
+        localStorage.clear();
+        meetings = []
 
-        if (currMoment > moment(currHour, 'h:a').hour()) {
-            $calTextEl.addClass('past');
-        } else if (currMoment < moment(currHour, 'h:a').hour()) {
-            $calTextEl.addClass('future');
-        } else {
-            $calTextEl.addClass('present');
-        }
+        for (let i = 9; i < 18; i++) {
+            let currHour = 0;
+            if (i > 12) {
+                currHour += i - 12;
+                currHour = currHour + 'pm';
+            } else {
+                currHour += i;
+                currHour = currHour + 'am';
+            }
 
-        $submitIconEl.addClass('mt-4 fas fa-upload');
+            const $rowEl = $('<div>');
+            const $calTimeEl = $('<div>');
+            const $calTextEl = $('<textarea>');
+            const $calSubmitEl = $('<div>');
+            const $submitIconEl = $('<i>');
 
-        $calSubmitEl.addClass('col-1 saveBtn')
-            .append($submitIconEl);
+            $calTimeEl.addClass('col-1 hour')
+                .text(currHour);
 
-        $rowEl.attr('id', i)
-            .addClass('row')
-            .append($calTimeEl, $calTextEl, $calSubmitEl);
+            $calTextEl.addClass('col-10 description');
 
-        $timeBlockEl.append($rowEl);
-    };
+            if (currMoment > moment(currHour, 'h:a').hour()) {
+                $calTextEl.addClass('past');
+            } else if (currMoment < moment(currHour, 'h:a').hour()) {
+                $calTextEl.addClass('future');
+            } else {
+                $calTextEl.addClass('present');
+            }
+
+            $submitIconEl.addClass('mt-4 fas fa-upload');
+
+            $calSubmitEl.addClass('col-1 saveBtn')
+                .append($submitIconEl);
+
+            $rowEl.attr('id', i)
+                .addClass('row')
+                .append($calTimeEl, $calTextEl, $calSubmitEl);
+
+            $timeBlockEl.append($rowEl);
+        };
+    }
 }
