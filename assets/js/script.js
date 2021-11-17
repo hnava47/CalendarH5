f9af55189748322f8e5ab6ea923762b2dcc4cfb7
Hector Nava <hector.r.nava44@gmail.com>
2021-11-16 22:02:57 -0700

    Added sample for loop for time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 const $currentDayEl = $('#currentDay');
+let meetings = JSON.parse(localStorage.getItem('meetings')) || [];)
 
 // inputs current date based on locale
 $currentDayEl.text(moment().format('dddd, MMMM Do'));
+
+let currentMeet = {
+    time: '9am',
+    details: 'test'
+};
+
+meetings.push(currentMeet);
+meetings.sort((a, b) => b.time - a.time);
+
+localStorage.setItem('meetings', JSON.stringify(meetings));
+
+for (let i = 9; i < 18; i++) {
+    let currentHour = 0
+    if (i > 12) {
+        currentHour += i - 12;
+    } else {
+        currentHour += i;
+    }
+    console.log(currentHour);
+}
